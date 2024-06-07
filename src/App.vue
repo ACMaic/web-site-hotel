eba6dc940b3cdc036253d5bce549ea7930db350a
ACMaic <maicon10mac@gmail.com>
2024-06-07 15:13:13 -0300

    setting

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar flat>
-      <v-container class="my-auto d-flex align-center justify-center">
-        <v-spacer></v-spacer>
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          :text="link"
-          variant="text"
-        ></v-btn>
-      </v-container>
-    </v-app-bar>
-
-    <v-main class="bg-grey-lighten-3">
+  <v-app>
+    <v-main>
       <v-container>
         <v-row>
-          <v-col cols="12">
-            <v-sheet min-height="70vh" rounded="lg" style="height: 100%;">
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
               <v-carousel height="100%" show-arrows="hover" cycle hide-delimiter-background>
                 <v-carousel-item v-for="(image, i) in roomData.property.propertyImages" :key="i">
                   <v-sheet height="700px">
@@ -26,51 +14,7 @@
               </v-carousel>
             </v-sheet>
           </v-col>
-          <v-col cols="4">
-            <v-sheet min-height="70vh" rounded="lg">
-              <v-carousel height="100%" show-arrows="hover">
-                <v-carousel-item v-for="(image, i) in roomData.images" :key="i">
-                  <v-sheet height="100%">
-                    <img :src="image.url" :alt="'Slide ' + (i + 1)" class="d-block w-100" />
-                  </v-sheet>
-                </v-carousel-item>
-              </v-carousel>
-              <v-card>
-                <v-card-title>{{ roomData.displayName }}</v-card-title>
-                <v-card-text>
-                  <p><strong>Address:</strong> {{ roomData.property.address }}</p>
-                  <p><strong>City:</strong> {{ roomData.property.city }}</p>
-                  <p><strong>Room Description:</strong> <span v-html="roomData.roomDescription"></span></p>
-                  <p><strong>Max People:</strong> {{ roomData.maxPeople }}</p>
-                  <p><strong>Rack Rate:</strong> ${{ roomData.rackRate }}</p>
-                  <p><strong>Cleaning Fee:</strong> ${{ roomData.cleaningFee }}</p>
-                </v-card-text>
-              </v-card>
-            </v-sheet>
-          </v-col>
-          <v-col cols="4" class="mx-auto">
-            <v-sheet min-height="70vh" rounded="lg">
-              <v-carousel height="100%" show-arrows="hover">
-                <v-carousel-item v-for="(image, i) in roomData.images" :key="i">
-                  <v-sheet height="100%">
-                    <img :src="image.url" :alt="'Slide ' + (i + 1)" class="d-block w-100" />
-                  </v-sheet>
-                </v-carousel-item>
-              </v-carousel>
-              <v-card>
-                <v-card-title>{{ roomData.displayName }}</v-card-title>
-                <v-card-text>
-                  <p><strong>Address:</strong> {{ roomData.property.address }}</p>
-                  <p><strong>City:</strong> {{ roomData.property.city }}</p>
-                  <p><strong>Room Description:</strong> <span v-html="roomData.roomDescription"></span></p>
-                  <p><strong>Max People:</strong> {{ roomData.maxPeople }}</p>
-                  <p><strong>Rack Rate:</strong> ${{ roomData.rackRate }}</p>
-                  <p><strong>Cleaning Fee:</strong> ${{ roomData.cleaningFee }}</p>
-                </v-card-text>
-              </v-card>
-            </v-sheet>
-          </v-col>
-          <v-col cols="4" class="mx-auto">
+          <v-col>
             <v-sheet min-height="70vh" rounded="lg">
               <v-carousel height="100%" show-arrows="hover">
                 <v-carousel-item v-for="(image, i) in roomData.images" :key="i">
@@ -99,47 +43,8 @@
 </template>
 
 <script setup>
-  import roomData from '../src/router/roomData.json'
+import { ref } from 'vue'
+import roomData from '../package.json'
 
-  const links = ['Dashboard', 'Messages', 'Profile', 'Updates']
-
-  const colors = [
-    'indigo',
-    'warning',
-    'pink darken-2',
-    'red lighten-1',
-    'deep-purple accent-4',
-  ]
-
-  const slides = [
-    'First',
-    'Second',
-    'Third',
-    'Fourth',
-    'Fifth',
-  ]
-</script>
-
-<script>
-  export default {
-    data() {
-      return {
-        links: ['Dashboard', 'Messages', 'Profile', 'Updates'],
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ]
-      }
-    }
-  }
+const slides = ref(['First', 'Second', 'Third', 'Fourth', 'Fifth'])
 </script>
